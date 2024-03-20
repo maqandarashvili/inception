5b70cc7ce57a1b2024b4a6305f98d94d4d7a91ec
Gia <maqandarashvili@gmail.com>
2024-03-20 12:21:29 +0400

    Some fixes

--- app/Http/Controllers/Api/PrizeController.php
+++ app/Http/Controllers/Api/PrizeController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\AssignPrizeToCategoryRequest;
 use App\Http\Requests\PrizeRequest;
 use App\Models\Category;
 use App\Models\Prize;
+use App\Models\PrizeLog;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -29,7 +30,36 @@ class PrizeController extends Controller
             'amount' => $request->amount,
             'odds' => $request->odds,
         ]);
-
         return response()->json(['message' => 'Prize assigned successfully'], Response::HTTP_OK);
+    }
+
+    public function spin(Request $request)
+    {
+        $user = $request->user();
+        $rank = $user->rank;
+
+        $prizes = $rank->prizes()->where('count', '>', 0)->get();
+
+        foreach ($prizes as $prize) {
+            if ($this->attemptWin($prize->probability)) {
+                $prize->count -= 1;
+                $prize->save();
+
+                PrizeLog::create([
+                    'user_id' => $user->id,
+                    'prize_id' => $prize->id,
+                    'won_at' => now(),
+                ]);
+
+                return response()->json(['won' => true, 'prize' => $prize->name]);
+            }
+        }
+
+        return response()->json(['won' => false, 'message' => 'Better luck next time!']);
+    }
+
+    protected function attemptWin($probability)
+    {
+        return mt_rand(1, 100) <= ($probability * 100);
     }
 }
--- app/Models/Prize.php
+++ app/Models/Prize.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Prize extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'type'];
 }
